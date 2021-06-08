94948014a539140637e800da4e446f3fddf35b76
parth <parthnvaswani@gmail.com>
2021-06-08 23:04:03 +0530

    hero section done

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// preloader
 const style = getComputedStyle(document.body);
 const timeout =
   (+style.getPropertyValue("--preload-delay").trim().slice(0, -1) +
@@ -7,4 +8,18 @@ setTimeout(() => {
   document
     .querySelector(".preload svg animateTransform")
     .setAttribute("dur", "0.9259259259259258s");
+  document.querySelector(".preload").addEventListener("click", () => {
+    window.location = "/";
+  });
 }, timeout);
+
+// hamburger
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector("header .links");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
